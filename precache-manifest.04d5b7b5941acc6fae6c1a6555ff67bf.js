64746c7c9241d3670dda6e37be3765e23209b1c2
Samarth <samarthbailur@gmail.com>
2020-01-30 11:01:31 +0530

    Updates

---
+++ precache-manifest.04d5b7b5941acc6fae6c1a6555ff67bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa5648b84be3e3d42e4c92d22d66a2fe",
+    "url": "/EmployeeList/index.html"
+  },
+  {
+    "revision": "15ec535c22761ec27f9e",
+    "url": "/EmployeeList/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "30acbcef7ffe463ed9d6",
+    "url": "/EmployeeList/static/js/2.f0221506.chunk.js"
+  },
+  {
+    "revision": "e2fc3609d47d90b8cc004bfe15ddf15e",
+    "url": "/EmployeeList/static/js/2.f0221506.chunk.js.LICENSE"
+  },
+  {
+    "revision": "15ec535c22761ec27f9e",
+    "url": "/EmployeeList/static/js/main.a4a1ccb7.chunk.js"
+  },
+  {
+    "revision": "29f476314e6e412b0446",
+    "url": "/EmployeeList/static/js/runtime-main.1a0192ba.js"
+  }
+]);
